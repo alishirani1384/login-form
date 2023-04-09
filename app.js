45b39ff2b2d18c9954d9e78d6b0093d5d60cfea0
Ali Shirani <alishiranii1384@gmail.com>
2023-04-09 15:44:46 +0330

    added providers

---
+++ app.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-app.js";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  GithubAuthProvider,
+} from "https://www.gstatic.com/firebasejs/9.19.1/firebase-auth.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCkIeuh0CHCissFZ-YHbUBNKPP54xsgSWA",
+  authDomain: "lgoin-form-v1.firebaseapp.com",
+  projectId: "lgoin-form-v1",
+  storageBucket: "lgoin-form-v1.appspot.com",
+  messagingSenderId: "2386873630",
+  appId: "1:2386873630:web:937d4d0d16ced3717b8d0f",
+};
+
+const app = initializeApp(firebaseConfig);
+
+const card = document.querySelector(".card h1");
+const name = document.querySelector(".card h1 span");
+const form = document.querySelector("form");
+
+const googleSignBtn = document.getElementById("google");
+const githubSignBtn = document.getElementById("github");
+
+googleSignBtn.addEventListener("click", () => {
+  const provider = new GoogleAuthProvider();
+  const auth = getAuth(app);
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      card.classList.remove("hidden");
+      form.classList.add("hidden");
+      name.innerHTML = user.displayName;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+});
+
+githubSignBtn.addEventListener("click", () => {
+  const provider = new GithubAuthProvider();
+  const auth = getAuth(app);
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      card.classList.remove("hidden");
+      form.classList.add("hidden");
+      name.innerHTML = user.displayName;
+      console.log(user);
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log(errorCode, errorMessage);
+    });
+});
